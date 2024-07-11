900ddbf16743e1cd883b22ef02b4beb8efd464b0
Felipe Augusto <felipe112412@gmail.com>
2024-07-11 11:29:32 -0300

    Atualização Desafio 3

--- Desafio-3-Alura/app.js
+++ Desafio-3-Alura/app.js
@@ -2,14 +2,39 @@
 
 function imc()
 {
+    let altura = prompt('Informe Sua altura');
+    let peso = prompt('Informe Seu peso');
+   
+    let imc= peso / (altura*altura);
 
+    alert(`Meu IMC é: ${imc}`);
+    console.log(`Meu IMC é: ${imc}`);
 }
 
 // 02) ----------------------
 
+function fatorial()
+{
+    let numero = prompt('Informe um numero');
+
+    for (var a = numero - 1; a >= 1; a--) 
+    {
+        numero = numero*a;
+    }
+    alert(`O fatorial deste numero é: ${numero}`);
+    console.log(`O fatorial deste numero é: ${numero}`);
+}
 
 // 03) ----------------------
 
+function conversao()
+{
+    let dolar = prompt('Informe o valor do dolar atual');
+    
+
+    alert(`O valor em reais é: ${dolar}`);
+    console.log(`O valor em reais é: ${dolar}`);
+}
 
 // 04) ----------------------
 
